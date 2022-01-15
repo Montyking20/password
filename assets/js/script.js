5a8d9d426587b81632dc30dbcbdd3e795ae9e88b
Chris <montyking20@gmail.com>
2022-01-15 14:32:09 -0700

    corrections

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,15 @@ let generateBtn = document.querySelector("#generate");
 
 // Password Characters
 function generatePassword(){
-  let lowCs = 'abcdefghijklmnopqrstuvwxyz';
-  let upCs = 'ABCDEFGHIJKLMNOPRSTUVWXYZ';
-  let numR = '1234567890';
-  let spCh = '~!@#$%^&*()_+{}:?><;.,';
-  let generatedPassword = '';
-  let chars = '';
+  lowCs = 'abcdefghijklmnopqrstuvwxyz';
+  upCs = 'ABCDEFGHIJKLMNOPRSTUVWXYZ';
+  numR = '1234567890';
+  spCh = '~!@#$%^&*()_+{}:?><;.,';
+  generatedPassword = '';
+  chars = '';
 
 // Password length
-let length = prompt('How many characters would you like your password length to be? \n 8 - 128 characters.')
+length = prompt('How many characters would you like your password length to be? \n 8 - 128 characters.')
 
 
 if (length >= 8 && length <= 128) {
@@ -22,10 +22,10 @@ return '';
 }
 
 // Character Prompts
-let checklowCs = confirm('Would you like to use lower case (a-z) letters?')
-let checkupCs = confirm('Would you like to use upper case (A-Z) letters?')
-let checknumR = confirm('Would you like to use numbers (0-9)?')
-let checkspCh = confirm('Would you like to use any special characters (!#$%&\')?')
+checklowCs = confirm('Would you like to use lower case (a-z) letters?')
+checkupCs = confirm('Would you like to use upper case (A-Z) letters?')
+checknumR = confirm('Would you like to use numbers (0-9)?')
+checkspCh = confirm('Would you like to use any special characters (!#$%&\')?')
 
 // Verify a character was chosen.
 if (checklowCs) {
@@ -54,8 +54,8 @@ return generatedPassword;
 
 // paste password into #password input
 function writePassword() {
-  let generatedPassword = generatePassword();
-  let passwordText = document.querySelector("#password");
+  generatedPassword = generatePassword();
+  passwordText = document.querySelector("#password");
 
   passwordText.value = generatedPassword;
 }
@@ -67,7 +67,7 @@ generateBtn.addEventListener("click", writePassword);
 
 function myFunction() {
   /* Get the text field */
-  var copyText = document.getElementById("password");
+  copyText = document.getElementById("password");
 
   /* Select the text field */
   copyText.select();
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Password-Generator 
 
 ## Requirements
-<p align="center">
+
 When generate a password button is clicked. 
 The user is presented with a series of prompts for password criteria.
 User selects which criteria to include in the password such as lowercase, uppercase, numeric, and/or special characters.
@@ -10,25 +10,25 @@ User is able to check choose a length of at least 8 characters and no more than 
 After all prompts are answered.
 A password is generated that matches the selected criteria.
 The password is either displayed in an alert or written to the page.
-</p>
+
 
 ## Completed
 Generate button generates prompts.
 Multiple prompts for password criteria.
 Password generated matching criteria is generated.
 
+
+
 ### Links
-<div align = center>
-
-<a href src= "https://github.com/Montyking20/password">GitHub<a>
 
 
-<a href src= "https://montyking20.github.io/password/">Password</a>
-</div>
+# [GitHub](https://github.com/Montyking20/password)
+
+
+### [Password](https://montyking20.github.io/password/)
+
 
 ## What it looks like
 
-<p align="center">
-<a img src= "..Screenshot-mainpage.jpg">
-<a img src= "/assets/images/Screenshot-mainpage.jpg">
-</P>
+
+![Password Generator](../Screenshot-mainpage.jpg)
